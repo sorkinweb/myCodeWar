911b077260cff1cbed2b0e468aff8a108d83c66e
sorkinfork <levykin.web@gmail.com>
2023-07-25 12:37:16 +0300

    8kyu. Century From Year

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 'use strict';
 
 /*
-    8kyu. Keep Hydrated!
+    8kyu. Century From Year
 
-Nathan loves cycling.
-Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
-You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
-For example:
-time = 3 ----> litres = 1
-time = 6.7---> litres = 3
-time = 11.8--> litres = 5
+The first century spans from the year 1 up to and including the year 100, the second century - 
+from the year 101 up to and including the year 200, etc.
 
+Task
+Given a year, return the century it is in.
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
 */
 
-function litres(time) {
-    return Math.floor(time * 0.5);
+function century(year) {
+    return Math.ceil(year / 100);
 }
