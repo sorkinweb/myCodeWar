d4c7f9c29b255565672cd6e25eabe75e98a8c889
sorkinfork <levykin.web@gmail.com>
2023-07-25 18:06:50 +0300

    8kyu. Calculate average

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 'use strict';
 
 /*
-    8kyu. Are You Playing Banjo?
+    8kyu. Calculate average
 
-Create a function which answers the question "Are you playing banjo?".
-If your name starts with the letter "R" or lower case "r", you are playing banjo!
-The function takes a name as its only argument, and returns one of the following strings:
-name + " plays banjo" 
-name + " does not play banjo"
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0.
 */
 
-function areYouPlayingBanjo(name) {
-    if(name.startsWith('R') || (name.startsWith('r'))) {
-        return name + ' plays banjo';
-    } else {
-        return name + ' does not play banjo';
-    }
+const newArr = [0, 0, 0];
+const newArr1 = [1, 1, 1];
+
+function findAverage(array) {
+    return array.length === 0 ? 0 : array.reduce((acc, el) => acc + el, 0) / array.length; 
 }
 
-console.log(areYouPlayingBanjo('Ricardo')); // Ricardo plays banjo
-console.log(areYouPlayingBanjo('Alexandr')); // Alexandr does not play banjo
-
-// or return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+console.log(findAverage(newArr)); // 0
+console.log(findAverage(newArr1)) // 1
