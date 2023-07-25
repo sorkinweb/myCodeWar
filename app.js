b12e849d1a74d827d932a6cb7ba154a9da1e7bfb
sorkinfork <levykin.web@gmail.com>
2023-07-25 12:02:23 +0300

    8kyu. Remove First and Last Character

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 /*
-    8kyu. Convert boolean values to strings 'Yes' or 'No'.
+    8kyu. Remove First and Last Character
 
-Complete the method that takes a boolean value 
-and return a "Yes" string for true, or a "No" string for false.
+
+    It's pretty straightforward. Your goal is to 
+    create a function that removes the first and last characters of a string. 
+    You're given one parameter, the original string. 
+    You don't have to worry with strings with less than two characters.
+
 */
 
-function boolToWord(value) {
-    return value ? 'Yes' : 'No';
+function removeChar(str) {
+    return str.slice(1, -1);
 }
