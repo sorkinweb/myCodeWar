52f89f3d72b90926edf1b2842d4d1f85fc4fed50
sorkinfork <levykin.web@gmail.com>
2023-07-25 12:40:33 +0300

    8kyu. Abbreviate a Two Word Name

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 'use strict';
 
 /*
-    8kyu. Century From Year
+    8kyu. Abbreviate a Two Word Name
 
-The first century spans from the year 1 up to and including the year 100, the second century - 
-from the year 101 up to and including the year 200, etc.
+Write a function to convert a name into initials. 
+This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
 
-Task
-Given a year, return the century it is in.
-Examples
-1705 --> 18
-1900 --> 19
-1601 --> 17
-2000 --> 20
+Sam Harris => S.H
+patrick feeney => P.F
 */
 
-function century(year) {
-    return Math.ceil(year / 100);
+function abbrevName(name) {
+    name = name.toUpperCase();
+    const [firstName, secondName] = name.split(' ');
+    return `${firstName[0]}.${secondName[0]}`;
 }
+
+// or return name.split(' ').map(i => i[0].toUpperCase()).join('.')
