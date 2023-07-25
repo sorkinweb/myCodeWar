99e679b04674a270fdfe0857f4e7dbecc98f4473
sorkinfork <levykin.web@gmail.com>
2023-07-25 12:07:26 +0300

    8kyu. Square(n) Sum

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
 
 /*
-    8kyu. String repeat
-    
+    8kyu. Square(n) Sum
 
-Write a function that accepts an integer n and a string s as parameters, 
-and returns a string of s repeated exactly n times.
-Examples (input -> output)
-6, "I"     -> "IIIIII"
-5, "Hello" -> "HelloHelloHelloHelloHello"
+Complete the square sum function so that it squares each number
+passed into it and then sums the results together.
 */
 
-function repeatStr(n, s) {
-    return s.repeat(n);
+function squareSum(numbers) {
+    const result = numbers
+        .map((el) => el * el)
+        .reduce((acc, el) => (acc += el), 0);
+    return result;
 }
+
+// or return numbers.reduce((sum,num) => sum + (num * num), 0);
