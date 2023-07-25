515ec6eadfee75db7b949ab4fab9e0160a15d89c
sorkinfork <levykin.web@gmail.com>
2023-07-25 11:58:13 +0300

    8kyu. Reversed Strings

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 'use strict';
 
 /*
-    8kyu. Sum of positive
+    8kyu. Reversed Strings
 
-You get an array of numbers, return the sum of all of the positives ones.
-Example [1,-4,7,12] => 1 + 7 + 12 = 20
-Note: if there is nothing to sum, the sum is default to 0.
+Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+'word'   =>  'drow'
 */
 
-function positiveSum(arr) {
-    let sum = 0;
-    const resultArr = arr.filter((el) => el > 0);
-    for (const el of resultArr) {
-        sum += el;
+function solution(str) {
+    if (typeof str == 'string') {
+        return str.split('').reverse().join('');
     }
-    return sum;
 }
-
-// or return arr.filter(element => element > 0).reduce((acc, el) => acc + el, 0);
