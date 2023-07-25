1903dfcd8d5781e0028bc452b089fcb2c849c598
sorkinfork <levykin.web@gmail.com>
2023-07-25 12:09:50 +0300

    8kyu. Grasshopper - Summation

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 
 /*
-    8kyu. Square(n) Sum
+    8kyu. Grasshopper - Summation
 
-Complete the square sum function so that it squares each number
-passed into it and then sums the results together.
+Write a program that finds the summation of every number from 1 to num. 
+The number will always be a positive integer greater than 0.
+For example (Input -> Output):
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 */
 
-function squareSum(numbers) {
-    const result = numbers
-        .map((el) => el * el)
-        .reduce((acc, el) => (acc += el), 0);
-    return result;
-}
+var summation = function (num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+};
 
-// or return numbers.reduce((sum,num) => sum + (num * num), 0);
+// or return num * (num + 1) / 2
