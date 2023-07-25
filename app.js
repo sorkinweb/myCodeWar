ffc366483ad42c9e36eecef2cf378f827157d4d7
sorkinfork <levykin.web@gmail.com>
2023-07-25 13:05:05 +0300

    8kyu. Is n divisible by x and y?

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 
 /*
-    8kyu. Convert number to reversed array of digits
+    8kyu. Is n divisible by x and y?
 
-Given a random non-negative number,
-you have to return the digits of this number within an array in reverse order.
-Example(Input => Output):
-35231 => [1,3,2,5,3]
-0 => [0]
+Create a function that checks if a number n is divisible by two numbers x AND y. 
+All inputs are positive, non-zero numbers.
+Examples:
+1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 */
 
-function digitize(n) {
-    return n.toString().split('').map((el) => +el).reverse();
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0;
 }
-
-// or return String(n).split('').map(Number).reverse()
