faec343f21333087cf77913a23c96e97695ae626
sorkinfork <levykin.web@gmail.com>
2023-07-30 12:28:48 +0300

    8kyu. Reduce but Grow

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 'use strict';
 
 /*
-    8kyu. Find Maximum and Minimum Values of a List
+    8kyu. Reduce but Grow
 
-Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) 
-that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
-Examples (Input -> Output)
-* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
-* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
-* [42, 54, 65, 87, 0]             -> min = 0, max = 87
-* [5]                             -> min = 5, max = 5
-Notes
-You may consider that there will not be any empty arrays/vectors.
+Given a non-empty array of integers, return the result of multiplying the values together in order. 
+Example:
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
 */
 
-const arrOfNum = [-52, 56, 30, 29, -54, 0, -110];
+const b = [1, 2, 3, 4];
 
-let min = function (list) {
-    return Math.min(...list);
-};
+function grow(x) {
+    return x.reduce((acc, el) => acc * el);
+}
 
-let max = function (list) {
-    return Math.max(...list);
-};
-
-console.log(min(arrOfNum)); // - 110
-console.log(max(arrOfNum)); // 56
+console.log(grow(b));
