867e1247738151e0923c4c2b3c7a36b322d7eeff
sorkinfork <levykin.web@gmail.com>
2023-07-30 14:07:17 +0300

    8kyu. Rock Paper Scissors!

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 'use strict';
 
 /*
-    8kyu. Convert a string to an array
+    8kyu. Rock Paper Scissors!
 
-"Robin Singh" ==> ["Robin", "Singh"]
-"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+Let's play! You have to return which player won! In case of a draw return Draw!.
+Examples(Input1, Input2 --> Output):
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
 
 */
 
-function stringToArray(string) {
-    return string.split(' ');
-}
+const rps = (p1, p2) => {
+    switch (true) {
+        case p1 == p2:
+            return 'Draw!';
+        case p1 == 'scissors' && p2 == 'paper':
+            return 'Player 1 won!';
+        case p1 == 'paper' && p2 == 'scissors':
+            return 'Player 2 won!';
+        case p1 == 'paper' && p2 == 'rock':
+            return 'Player 1 won!';
+        case p1 == 'rock' && p2 == 'paper':
+            return 'Player 2 won!';
+        case p1 == 'scissors' && p2 == 'rock':
+            return 'Player 2 won!';
+        case p1 == 'rock' && p2 == 'scissors':
+            return 'Player 1 won!';
+    }
+};
+console.log(rps('paper', 'paper'));
 
-console.log(stringToArray('Robin Singh')); // ['Robin', 'Singh']
+
+/*
+
+or: 
+const rps1 = (p1, p2) => {
+    const rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
+    if (p1 === p2) return 'Draw!';
+    if (p2 === rules[p1]) {
+        return 'Player 1 won!';
+    } else {
+        return 'Player 2 won!';
+    }
+};
+
+console.log(rps1('rock', 'scissors'));
+
+*/
