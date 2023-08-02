fbea6c326ddcb088e80b92665f2a24e9943d31cb
sorkinfork <levykin.web@gmail.com>
2023-08-02 16:37:35 +0300

    8kyu. All Star Code Challenge #18

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 'use strict';
 
 /*
-    8kyu. Removing Elements
+    8kyu. All Star Code Challenge #18
 
-Take an array and remove every second element from the array. 
-Always keep the first element and start removing with the next element.
-Example:
-["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
-None of the arrays will be empty, so you don't have to worry about that!
+Create a function that accepts a string and a single character, 
+and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+If no occurrences can be found, a count of 0 should be returned.
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
 
+The first argument can be an empty string
+In languages with no distinct character data type, the second argument will be a string of length 1
 */
 
-function removeEveryOther(arr) {
-    return arr.filter((_, index) => index % 2 === 0);
+function strCount(str, letter) {
+    let resultArr = [];
+    for(const el of str) {
+        if(el === letter) {
+            resultArr.push(el);
+        }
+    }
+    return resultArr.length;
 }
 
-console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"])); // ['Keep', 'Keep', 'Keep']
+console.log(strCount('Hello', 'o')) // 1
+console.log(strCount('Hello', 'l')); // 2
+
+// or return str.split(letter).length-1  or return str.split('').filter(c => c == letter).length;
