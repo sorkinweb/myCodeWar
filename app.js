5eecd608a37687606229ce1883c0349923397dad
sorkinfork <levykin.web@gmail.com>
2023-08-02 17:29:08 +0300

    8kyu. Find numbers which are divisible by given number

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 'use strict';
 
 /*
-    8kyu. Filter out the geese
+    8kyu. Find numbers which are divisible by given number
 
-Write a function that takes a list of strings as an argument 
-and returns a filtered list containing the same elements but with the 'geese' removed.
-The geese are any strings in the following array, which is pre-populated in your solution:
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
+First argument is an array of numbers and the second is the divisor.
 
-["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
-For example, if this array were passed as an argument:
-
-["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
-Your function would return the following array:
-
-["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
-The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. 
-Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
 
 */
 
-function gooseFilter (birds) {
-    let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    return birds.filter(el => !geese.includes(el));
-};
+function divisibleBy(numbers, divisor) {
+    return numbers.filter(el => el % divisor === 0);
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // [2, 4, 6]
