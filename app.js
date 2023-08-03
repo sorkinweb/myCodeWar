69086cdf9f0c1b749b3dd1de407c86eca58e4775
sorkinfork <levykin.web@gmail.com>
2023-08-03 14:47:25 +0300

    8kyu. Stringy Strings

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 'use strict';
 
 /*
-    8kyu. Find numbers which are divisible by given number
+    8kyu. Stringy Strings
 
-Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
-First argument is an array of numbers and the second is the divisor.
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
 
-Example(Input1, Input2 --> Output)
-[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+the string should start with a 1.
+a string with size 6 should return :'101010'.
+with size 4 should return : '1010'.
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
 
 */
 
-function divisibleBy(numbers, divisor) {
-    return numbers.filter(el => el % divisor === 0);
+function stringy(size) {
+    let string = '';
+    for(let i = 0; i < size; i++) {
+        if (i % 2 == 0) {
+            string += 1;
+        } else {
+            string += 0;
+        }
+    }
+    return string;
 }
 
-console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // [2, 4, 6]
+console.log(stringy(6)); // 101010
+
+// or return ''.padStart(size, '10');
