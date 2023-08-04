1de80a5831f71250f06b2aafe18a62693705804f
sorkinfork <levykin.web@gmail.com>
2023-08-04 16:49:23 +0300

    8kyu. Bin to Decimal

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 'use strict';
 
 /*
-    8kyu. Add Length
+    8kyu. Bin to Decimal
 
-What if we need the length of the words separated by a space 
-to be added at the end of that same word and have it returned as an array?
-Example(Input --> Output)
-"apple ban" --> ["apple 5", "ban 3"]
-"you will win" -->["you 3", "will 4", "win 3"]
-
-Your task is to write a function that takes a String
-and returns an Array/list with the length of each word added to each element .
-Note: String will have at least one element; words will always be separated by a space.
+Complete the function which converts a binary number (given as a string) to a decimal number.
 */
 
-
-function addLength(str) {
-    return str.split(' ').map(el => el + ' ' + el.length);
+function binToDec(bin) {
+    return parseInt(bin, 2);
 }
 
-console.log(addLength('Interesting Task BTW')); // ['Interesting 11', 'Task 4', 'BTW 3']
-
+console.log(binToDec('1001001')); // 73
