e43c2a6ca29293d89c1a42d345ab3d2616ce78c7
sorkinfork <levykin.web@gmail.com>
2023-08-04 17:07:14 +0300

    8kyu. No zeros for heros

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 'use strict';
 
 /*
-    8kyu. Bin to Decimal
+    8kyu. No zeros for heros
 
-Complete the function which converts a binary number (given as a string) to a decimal number.
+Numbers ending with zeros are boring.
+They might be fun in your world, but not here.
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+
+Zero alone is fine, don't worry about it. Poor guy anyway
 */
 
-function binToDec(bin) {
-    return parseInt(bin, 2);
+function noBoringZeros(n) {
+    while (n % 10 == 0 && n != 0) {
+        n = n / 10;
+    }
+    return n;
 }
 
-console.log(binToDec('1001001')); // 73
+console.log(noBoringZeros(500)); // 5
+
+// or return +`${n}`.replace(/0*$/, "");
