d752c2174d8b5d533e74f8c50da7e31d4c8a711d
sorkinfork <levykin.web@gmail.com>
2023-08-14 14:52:13 +0300

    7kyu. Square Every Digit

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 'use strict';
 
 /*
-    7kyu. Disemvowel Trolls
+    7kyu. Square Every Digit
 
-Trolls are attacking your comment section!
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-Your task is to write a function that takes a string and return a new string with all vowels removed.
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-Note: for this kata y isn't considered a vowel.
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+Note: The function accepts an integer and returns an integer.
+Happy Coding!
 */
 
-function disemVowel(str) {
-    const vowels = 'aeiou'
-    return str.split('').filter(el => !vowels.includes(el.toLowerCase())).join('');
+function squareDigits(num) {
+    return +('' + num).split('').map(el => el ** 2).join('');
 }
-
-console.log(disemVowel('This website is for losers LoL!')); // Ths wbst s fr lsrs LL!
-
-// or regex return str.replace(/[aeiou]/gi, '');
+console.log(squareDigits(2112)); // 4114
