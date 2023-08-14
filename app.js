252d046f4978a1f52188bb06782cef8455c2b7f8
sorkinfork <levykin.web@gmail.com>
2023-08-14 15:19:56 +0300

    7kyu. Highest and Lowest

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 'use strict';
 
 /*
-    7kyu. Square Every Digit
+    7kyu. Highest and Lowest
 
-Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
-Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
-Note: The function accepts an integer and returns an integer.
-Happy Coding!
+In this little assignment you are given a string of space separated numbers, 
+and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
 */
 
-function squareDigits(num) {
-    return +('' + num).split('').map(el => el ** 2).join('');
+function highAndLow(numbers) {
+    const resultArr = numbers.split(' ').sort((a, b) => a - b);
+    let firstNum = resultArr[0];
+    let lastNum = resultArr[resultArr.length-1];
+    return `${lastNum} ${firstNum}`;
 }
-console.log(squareDigits(2112)); // 4114
+
+console.log(highAndLow('1 2 3 4 5 6 8 12 22 7')) // 22 1
+
+// or   numbers = numbers.split(' '); return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
