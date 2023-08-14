703e389381d191930cd7c35c74a499f0a94ef017
sorkinfork <levykin.web@gmail.com>
2023-08-14 18:27:48 +0300

    7kyu. You're a square!

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 'use strict';
 
 /*
-    7kyu. Highest and Lowest
+    7kyu. You're a square!
 
-In this little assignment you are given a string of space separated numbers, 
-and have to return the highest and lowest number.
+You like building blocks. You especially like building blocks that are squares. 
+And what you even like more, is to arrange them into a square of square building blocks!
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! 
+Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! 
+That's it! You just have to check if your number of building blocks is a perfect square.
 
-Examples
-highAndLow("1 2 3 4 5");  // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
+In mathematics, a square number or perfect square is an integer that is the square of an integer; 
+in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
 */
 
-function highAndLow(numbers) {
-    const resultArr = numbers.split(' ').sort((a, b) => a - b);
-    let firstNum = resultArr[0];
-    let lastNum = resultArr[resultArr.length-1];
-    return `${lastNum} ${firstNum}`;
-}
+const isSquare = function (n) {
+    return Math.sqrt(n) % 1 === 0;
+};
 
-console.log(highAndLow('1 2 3 4 5 6 8 12 22 7')) // 22 1
-
-// or   numbers = numbers.split(' '); return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+console.log(isSquare(144)); // true
