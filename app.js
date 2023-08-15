2feca700df79cd20dc0b4ce743eac31bf24ff672
sorkinfork <levykin.web@gmail.com>
2023-08-15 12:12:34 +0300

    7kyu. List Filtering

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 'use strict';
 
 /*
-    7kyu. Get the Middle Character
+    7kyu. List Filtering
 
-You are going to be given a word. Your job is to return the middle character of the word. 
-If the word's length is odd, return the middle character. 
-If the word's length is even, return the middle 2 characters.
-#Examples:
+In this kata you will create a function that takes a list of non-negative integers and strings 
+and returns a new list with the strings filtered out.
 
-Kata.getMiddle("test") should return "es"
-Kata.getMiddle("testing") should return "t"
-Kata.getMiddle("middle") should return "dd"
-Kata.getMiddle("A") should return "A"
-
-A word (string) of length 0 < str < 1000 
-(In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). 
-You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
-
-#Output
-The middle character(s) of the word represented as a string.
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
 
-function getMiddle(s) {
-    return s.slice((s.length - 1) / 2, s.length / 2 + 1);
+function filter_list(l) {
+  return l.filter(el => typeof(el) === 'number');
 }
 
-console.log(getMiddle('middle')); // dd
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123])); // [1, 2, 123];
+
+// or return l.filter(e => Number.isInteger(e));
