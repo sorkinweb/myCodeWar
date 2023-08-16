9a47212ac0f3796b5d6f2be3db4411374762fe09
sorkinfork <levykin.web@gmail.com>
2023-08-16 18:09:10 +0300

    7kyu. Growth of a Population

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 'use strict';
 
 /*
-    7kyu. Sum of odd numbers
+    7kyu. Growth of a Population
 
-Given the triangle of consecutive odd numbers:
+In a small town the population is p0 = 1000 at the beginning of a year. 
+The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. 
+How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
 
-             1
-          3     5
-       7     9    11
-   13    15    17    19
-21    23    25    27    29
-...
+At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
 
-Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: 
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
 
-(Input --> Output)
-1 -->  1
-2 --> 3 + 5 = 8
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+
+Note:
+Don't forget to convert the percent parameter as a percentage in the body of your function: 
+if the parameter percent is 2 you have to convert it to 0.02.
 */
 
-function rowSumOddNumbers(n) {
-    return Math.pow(n, 3);
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    for (years; p0 < p; years++) {
+        p0 += p0 * (percent / 100) + aug | 0;
+    }
+    return years;
 }
 
-console.log(rowSumOddNumbers(42)); // 74088
+console.log(nbYear(1500, 5, 100, 5000)); // 15
