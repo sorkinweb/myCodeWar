882f3b3dcbc2531ee4f27d5a4d2c08c9e1dc6812
sorkinfork <levykin.web@gmail.com>
2023-08-16 17:03:54 +0300

    7kyu. Sum of odd numbers

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 'use strict';
 
 /*
-    7kyu. Two to One
+    7kyu. Sum of odd numbers
 
-Take 2 strings s1 and s2 including only letters from a to z. 
-Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+Given the triangle of consecutive odd numbers:
 
-a = "xyaabbbccccdefww"
-b = "xxxxyyyyabklmopq"
-longest(a, b) -> "abcdefklmopqwxy"
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
 
-a = "abcdefghijklmnopqrstuvwxyz"
-longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: 
+
+(Input --> Output)
+1 -->  1
+2 --> 3 + 5 = 8
 */
 
-function longest(s1, s2) {
-    s1 = s1.split('');
-    s2 = s2.split('');
-    return s1.concat(s2).reduce((acc, el) => {
-        if (acc.includes(el)) {
-            return acc;
-        }
-        return [...acc, el].sort().join('');
-    })
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
 }
 
-console.log(longest('xyaabbbccccdefww', 'xxxxyyyyabklmopq')); // abcdefklmopqwxy
-
-// or const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
-// or return Array.from(new Set(s1 + s2)).sort().join('');
+console.log(rowSumOddNumbers(42)); // 74088
