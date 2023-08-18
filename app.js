75a1e61964eefe6e2a5b95e6afec224605b9b936
sorkinfork <levykin.web@gmail.com>
2023-08-18 15:25:47 +0300

    7kyu. Odd or Even?

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 'use strict';
 
 /*
-    7kyu. Reverse words
+    7kyu. Odd or Even?
 
-Complete the function that accepts a string parameter, and reverses each word in the string. 
-All spaces in the string should be retained.
+Given a list of integers, determine whether the sum of its elements is odd or even.
 
-"This is an example!" ==> "sihT si na !elpmaxe"
-"double  spaces"      ==> "elbuod  secaps"
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
 */
 
-function reverseWords(str) {
-    return str.split('').reverse().join('').split(' ').reverse().join(' ');
+function oddOrEven(array) {
+    if(array.length === 0) {
+        return 'even'
+    } else {
+        let result = array.reduce((acc, el) => acc += el);
+        return result % 2 === 0 ? 'even' : 'odd';
+    }
 }
 
-console.log(reverseWords('this is an example!')) // siht si na !elpmaxe
+console.log(oddOrEven([0, 1, 3])); // even
+console.log(oddOrEven([0, 1, 4])); // odd
